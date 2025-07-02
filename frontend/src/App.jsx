fbfee5c12e5dc3f85f013c71e4b1928e90493db6
Logan Houp <lhoup@usc.edu>
2025-07-02 16:31:37 -0700

    fix dark mode text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import Game from "./Game"
 
 function App() {
   return (
-    <div className="fixed inset-0 w-screen h-screen grid place-items-center bg-gradient-to-br from-blue-100 to-purple-100">
+    <div className="fixed inset-0 w-screen h-screen grid place-items-center bg-gradient-to-br from-blue-100 to-purple-100 text-gray-900">
       <Game />
     </div>
   )
